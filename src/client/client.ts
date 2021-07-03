4fc5d105b8abedfaea83d780f932af9daac8ebd1
NathanInnes <NathanInnes@users.noreply.github.com>
2021-07-03 05:16:44 +0000

    deploy: a3b3dd727018970d333f82b5883b03e344388f66

---
+++ src/client/client.ts
@@ -0,0 +1,29 @@
+const controlF6 = 167
+
+setTick(() => {
+    DisableControlAction(0, controlF6, true);
+});
+
+export function EngineToggle() {
+    RegisterCommand(
+        'engine',
+        async () => {
+            handleEngineToggle();
+        },
+        false
+    );
+    if(IsControlJustReleased(0,controlF6) || IsDisabledControlJustReleased(0,controlF6)) {
+        handleEngineToggle();
+    }
+}
+
+  function handleEngineToggle() {
+    const vehicle = GetVehiclePedIsIn(PlayerPedId(), false);
+    const isRunning = Number(GetIsVehicleEngineRunning(vehicle));
+    if (vehicle) {
+      SetVehicleEngineOn(vehicle, !isRunning, false, true);
+      console.log(`Engine is now ${isRunning}`)
+    } else {
+      emit('chat:addMessage', 'You are not inside a vehicle.')
+    }
+  }
